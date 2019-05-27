7b17f80d316649877f6052ae61f4bc7038d28ed2
wailovet <wailovet@163.com>
2019-05-27 14:21:41 +0800

    init

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ sudo pacman -Syy
 sudo pacman -Syu
 sudo pacman -Syyu
 sudo pacman -S yay
+
 yay -S --noconfirm shadowsocks-qt5
-yay -S --noconfirm fcitx-im    #默认全部安装
+
+yay -S --noconfirm fcitx-im
 yay -S --noconfirm fcitx-configtool
 yay -S --noconfirm fcitx-googlepinyin
 echo export GTK_IM_MODULE=fcitx >>  ~/.xprofile
@@ -14,3 +16,14 @@ echo export 'XMODIFIERS="@im=fcitx"' >>  ~/.xprofile
 yay -S --noconfirm google-chrome
 
 yay -S --noconfirm filezilla
+
+yay -S --noconfirm rdesktop
+
+yay -S --noconfirm go
+
+echo export PATH=$PATH:$(go env GOPATH)/bin >> ~/.profile
+
+echo export GOPATH=$(go env GOPATH) >> ~/.profile
+
+go get -v github.com/wailovet/cmdgui
+go install github.com/wailovet/cmdgui
